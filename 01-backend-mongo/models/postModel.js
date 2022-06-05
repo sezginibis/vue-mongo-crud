9ca835c1f1acc614c221bca5e8416167e0206662
Sezgin İbiş (incelemeelemani) <34094261+sezginibis@users.noreply.github.com>
2022-06-05 07:17:36 +0300

    Create postModel.js

---
+++ 01-backend-mongo/models/postModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+module.exports = mongoose.model('Posts', PostSchema); // Burada kullandığın isim tablonun adı
