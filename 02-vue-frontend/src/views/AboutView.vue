335861ae09fc02d66ca8796caaac7586ad8166a8
Sezgin İbiş (incelemeelemani) <34094261+sezginibis@users.noreply.github.com>
2022-06-05 07:21:31 +0300

    Create AboutView.vue

---
+++ 02-vue-frontend/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1 class="text-center">This is an about page</h1>
+    <h3>Front-end</h3>
+    <ul>
+      <li><s>Title için props oluşturulacak.</s></li>
+      <li>Validation'lar yapılmadı.</li>
+      <li>Bootstrap çok paspal, değişmesi gerekir.</li>
+      <li>Sayfalandırma yapılmadı</li>
+      <li>Silmede, güncellemede ve yeni eklemede sayfanın yeniden yükleniyor gibi olması konusunda araştırma yapılmalı.</li>
+      <li>Resim yükleme üzerinde çalışılmalı.</li>
+      <li>Aslında Post'u personele çevirip, ardından yolculuk oluşturulmalı fakat bunun için henüz erken olduğunu düşünüyorum. Öncelikle dört öğesi bulunan Post için bir Comments oluşturularak, nosql'in relation'larına bakılmalı.</li>
+    </ul> 
+    <h3>Back-end</h3>
+    <ul>
+      <li>Kullanıcı girişi ve yetkilendirme yapılmalı.</li>
+    </ul>
+  </div>
+</template>
+<style>
+.about {
+  text-align: left;
+}
+</style>
