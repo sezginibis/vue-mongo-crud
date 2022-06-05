e616d7d7555303e7a6d49ca5a72246bc4f6c00f2
Sezgin İbiş (incelemeelemani) <34094261+sezginibis@users.noreply.github.com>
2022-06-05 07:24:50 +0300

    Create PostTitle.vue

---
+++ 02-vue-frontend/src/components/posts/PostTitle.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: 'This is an post api page',
+    }
+});
+
+</script>
+<template>
+<div class="mb-2">
+    <h1>{{title}}</h1>
+    <div class="d-grid gap-2 mt-4">
+        <router-link to="/post-create" class="btn btn-primary">New Post Add</router-link>
+    </div>
+</div>
+</template>
