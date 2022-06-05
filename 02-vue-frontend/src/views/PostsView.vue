47dfdca1313551e509f101096dec7b52e6455085
Sezgin İbiş (incelemeelemani) <34094261+sezginibis@users.noreply.github.com>
2022-06-05 07:22:08 +0300

    Create PostsView.vue

---
+++ 02-vue-frontend/src/views/PostsView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PostIndexVue from '@/components/posts/PostIndex.vue';
+import PostTitleVue from '@/components/posts/PostTitle.vue';
+import PostTitle from '@/components/posts/PostTitle.vue';
+import { ref } from 'vue';
+
+const title = ref('This is an post list page');
+</script>
+
+<template>
+    <div>
+        <PostTitleVue :title="title" />
+        <PostIndexVue />
+    </div>
+</template>
